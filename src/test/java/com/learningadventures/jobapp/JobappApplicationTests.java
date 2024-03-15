ba1391ccccccb86191cee1a86210ed8a0c421fba
LearningAdventuresWithNitu <142468489+LearningAdventuresWithNitu@users.noreply.github.com>
2024-03-14 22:22:03 -0400

    first commit

---
+++ src/test/java/com/learningadventures/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningadventures.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
